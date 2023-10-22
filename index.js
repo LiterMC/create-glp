b4102fc2d3b51de00d1216db0b8c733ad3d6c9dd
zyxkad <zyxkad@gmail.com>
2023-10-22 05:21:28 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+const create = require("base-create");
+
+create({
+	dependencies: [
+		"go-liter-plugin": "^1.0.0",
+	],
+	devDependencies: [
+		"typescript",
+		"@andersdjohnson/tsconfig",
+		"npm-build-zip",
+	],
+	package: {
+		files: ["dist"],
+		main: "dist/index.js",
+		types: "dist/index.d.ts",
+		keywords: ["go-liter-plugin"],
+		scripts: {
+			"check": "tsc --noEmit",
+			"zip": "mkdir -p dist && npm-build-zip --destination=dist",
+			"build": "npm run build:types && npm run build:ts && npm run zip",
+			"build:ts": "tsc",
+			"build:types": "tsc --emitDeclarationOnly",
+			"build:watch": "npm run build -- --watch",
+		},
+	},
+	files: [
+		{
+			path: "tsconfig.json",
+			contents: {
+				"compilerOptions": {
+					"module": "commonjs",
+					"target": "es6",
+					"lib": ["es6"],
+					"declaration": true,
+					"noImplicitAny": true,
+					"preserveConstEnums": true,
+					"removeComments": true,
+					"sourceMap": true,
+					"outDir": "build",
+				},
+				"include": [
+					"node_modules/go-liter-plugin",
+					"src",
+				],
+			},
+		},
+		"src/index.ts",
+	],
+})
