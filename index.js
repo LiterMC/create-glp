fa44acfd3729b0640c2a7954abecb94c702435af
zyxkad <zyxkad@gmail.com>
2023-10-23 14:01:54 -0600

    add meta exporter

--- index.js
+++ index.js
@@ -13,19 +13,95 @@ create({
 	],
 	package: {
 		files: ["dist"],
-		main: "dist/index.js",
-		types: "dist/index.d.ts",
-		keywords: ["go-liter-plugin"],
+		main: "index.js",
+		types: "index.d.ts",
+		keywords: ["go-liter"],
 		scripts: {
-			"check": "tsc --noEmit",
-			"zip": "mkdir -p dist && npm-build-zip --destination=dist",
-			"build": "npm run build:types && npm run build:ts && npm run zip",
-			"build:ts": "tsc",
-			"build:types": "tsc --emitDeclarationOnly",
-			"build:watch": "npm run build -- --watch",
-		},
+	    "test": "tsc --noEmit",
+	    "export-meta": "node cmd/export-meta",
+	    "zip": "npm-build-zip --destination=dist",
+	    "build": "npm run export-meta && npm run build:types && npm run build:ts && npm run zip",
+	    "build:ts": "tsc",
+	    "build:types": "tsc --emitDeclarationOnly",
+	    "build:watch": "npm run build -- --watch"
+	  },
 	},
 	files: [
+		{
+			path: ".gitignore",
+			contents: `
+# Mac OS
+.DS_Store
+
+# builds
+build/
+dist/
+
+# Logs
+logs
+*.log
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+lerna-debug.log*
+.pnpm-debug.log*
+
+# Diagnostic reports (https://nodejs.org/api/report.html)
+report.[0-9]*.[0-9]*.[0-9]*.[0-9]*.json
+
+# Runtime data
+pids
+*.pid
+*.seed
+*.pid.lock
+
+# Dependency directories
+node_modules/
+jspm_packages/
+
+# TypeScript cache
+*.tsbuildinfo
+
+# Optional npm cache directory
+.npm
+
+# Optional eslint cache
+.eslintcache
+
+# Optional stylelint cache
+.stylelintcache
+
+# Optional REPL history
+.node_repl_history
+
+# Output of 'npm pack'
+*.tgz
+
+# Yarn Integrity file
+.yarn-integrity
+
+# dotenv environment variable files
+.env
+.env.development.local
+.env.test.local
+.env.production.local
+.env.local
+
+# parcel-bundler cache (https://parceljs.org/)
+.cache
+.parcel-cache
+
+# Stores VSCode versions used for testing VSCode extensions
+.vscode-test
+
+# yarn v2
+.yarn/cache
+.yarn/unplugged
+.yarn/build-state.yml
+.yarn/install-state.gz
+.pnp.*
+`
+		}
 		{
 			path: "tsconfig.json",
 			contents: {
@@ -49,6 +125,38 @@ create({
 				],
 			},
 		},
-		"src/index.ts",
+		{
+			path: "cmd/export-meta.js",
+			contents: `
+const fs = require('fs')
+
+fs.rmSync('build', { recursive: true, force: true })
+fs.mkdirSync('build')
+if (!fs.existsSync('dist')){
+	fs.mkdirSync('dist')
+}
+
+const packageJson = JSON.parse(fs.readFileSync('package.json'))
+
+const meta = {
+	"name": packageJson["name"],
+	"version": packageJson["version"],
+	"description": packageJson["description"],
+	"dependencies": packageJson["dependencies"],
+}
+
+fs.writeFileSync('build/plugin.meta.json', JSON.stringify(meta), 'utf-8')
+`
+		},
+		{
+			path: "src/index.ts",
+			contents: `
+console.log(console.log(\`plugin ${$.ID}@${$.VERSION} is loading\`))
+
+$.on('unload', () => {
+	console.log('plugin %q is unloading', $.ID)
+})
+`
+		}
 	],
 })
